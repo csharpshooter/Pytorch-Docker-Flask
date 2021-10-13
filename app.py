20f6b9ab6980f08bda35115a12b8fb179d1140fb
csharpshooter <abhijitmali@hotmail.com>
2021-10-13 19:34:14 +0530

    updated to show multiple predictions

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ import traceback,sys,os
 from service_streamer import ThreadedStreamer
 import re
 
-infer = ("{% extends \'base.html' %}"+
+infer_baseblock = ("{% extends 'base.html' %}"+
 "{% block content %}"+
-"<p class=\"lead\">File(s) uploaded successfully." +
-"You have uploaded an image of</p> <h1 class=\"display-4\">{{val1}}!</h1> <p class=\"mt-5\">Confidence: <strong>{{val2}}%</strong></p> <p class=\"mt-5\">" +
-"<a href=\"\\\">Try again</a></p>"+
+"<div class=\"pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center\">"+
+"<p class=\"lead\">File(s) uploaded successfully. You have uploaded an image of</p>")
+
+infer = (
+" <h3 class=\"display-4\">{{val1}}!</h3> <p class=\"mt-5\">Confidence: <strong>{{val2}}%</strong></p> <p class=\"mt-5\">" +
+"<img class=\"mb-4\" src=\"data:image/jpeg;base64,{{ img_data }}\" alt=\"\" width=\"300\">"
+)
+
+infer_endblock = ("<a href=\"\\\">Try again</a></p>"+
+"<//div>" +
 "{% endblock %}")
 
 app = Flask(__name__)
@@ -40,17 +47,20 @@ def success():
                     saveLocation.append(f.filename)
                     # saveLocation = f.filename
                     f.save(f.filename)
-                result = streamer.predict(saveLocation)
-                # result = model.infer(saveLocation)
-                print(result)
+                results = streamer.predict(saveLocation)
+                print(results)
 
-                if len(result) == 1:
-                    print("single")
-                    
-                    return render_template_string(infer,val1=result[0][0],val2=result[0][1])
-                else:
-                    print("multiple")
-                
+                output = ""
+
+                if len(results) == 1:
+                    print("single")                    
+                    output = render_template_string(infer_baseblock+infer+infer_endblock,val1=results[0][0],val2=results[0][1])
+                else:                    
+                    for result in results:
+                        output += infer.replace("{{val1}}",result[0]).replace("{{val2}}",result[1])
+                    output = render_template_string(infer_baseblock+output+infer_endblock)
+
+                return output             
     except :
             return jsonify({'error': traceback.print_exception(*sys.exc_info())})
 
