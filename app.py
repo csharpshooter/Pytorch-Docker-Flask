cf3ca191c77666ca80c034c795fe8ca614158662
csharpshooter <abhijitmali@hotmail.com>
2021-10-22 21:54:29 +0530

    update html and last inference display

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, request,jsonify,render_template_string
+from flask import Flask, render_template, request,jsonify,render_template_string,url_for
 from models import MobileNet
 import os
 from math import floor
 import io
 import traceback,sys,os
 from service_streamer import ThreadedStreamer
-import re
+from PIL import Image
+import base64,uuid, datetime,glob
+import pandas as pd
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+app.config['STATIC'] = "./static/"
+app.config['UPLOAD_FOLDER'] = os.path.join(app.config['STATIC'],'uploads/')    # To host static images
+app.config['datalog'] = os.path.join(app.config['STATIC'], 'data/data.csv')
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # Limit the memory to 10 MB
+
+columns = ["Inference Id","Prediction","Confidence","Timestamp"]
 
 infer_baseblock = ("{% extends 'base.html' %}"+
 "{% block content %}"+
@@ -14,21 +27,52 @@ infer_baseblock = ("{% extends 'base.html' %}"+
 
 infer = (
 " <h3 class=\"display-4\">{{val1}}!</h3> <p class=\"mt-5\">Confidence: <strong>{{val2}}%</strong></p> <p class=\"mt-5\">" +
-"<img class=\"mb-4\" src=\"data:image/jpeg;base64,{{ img_data }}\" alt=\"\" width=\"300\">"
+"<img {{ url_for(\"static\", filename=\"img_data\") }} alt=\"\">"
+)
+
+# <img class="mb-4" src="data:image/jpeg;base64,{{ img_data }}" alt="" width="300">
+# <img src="data:image/jpeg;base64,{{ img_data }}" alt="">
+
+last_infer = (
+    "<h2>Last 5 Inference Results</h2>"+
+    "<table>"+
+ "<thead>"+
+  "{%- for column in columns %}"+
+     "<th>{{ column }}</th>"+
+  "{%- endfor %}"+
+ "</thead>"+
+
+ "<tbody>"+
+ "{%- for row in items %}"+
+    "<tr>"+
+    "{%- for column in columns %}"+
+       "<td>{{ row|attr(column) }}</td>"+
+    "{%- endfor %}"+
+    "</tr>"+
+ "{%- endfor %}"+
+ "</tbody>"+
+ "</table>"
 )
 
 infer_endblock = ("<a href=\"\\\">Try again</a></p>"+
+ "<h5 class=\"center\">Last 5 Inference Results</h5>"+
 "<//div>" +
+ "<div  class=\"center\">{{last5}}<//div>"+
 "{% endblock %}")
 
-app = Flask(__name__)
 model = MobileNet()
 saveLocation = ""
 app.config['UPLOAD_FOLDER'] = 'uploads'
 streamer = ThreadedStreamer(model.infer, batch_size=64)
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    df = pd.read_csv('./static/data/data.csv')
+    df1 = df.tail(5)
+    index_html = Path('./templates/index.html').read_text()    
+    return render_template_string(index_html.replace('{{last_5_infer}}',df1.to_html(justify='center',index=False)).replace("<table","<table align=\"center\""))
 
 
 @app.route('/about')
@@ -40,9 +84,13 @@ def about():
 def success():
     try:
         if request.method == 'POST':
+            # print(static_url_path)
+            df = pd.read_csv('./static/data/data.csv')
             saveLocation = []
+            # filedata = []
             if 'file' in request.files:
                 for f in request.files.getlist('file'):
+                    # filedata.append(f)
                     print(f.filename)  
                     saveLocation.append(f.filename)
                     # saveLocation = f.filename
@@ -52,13 +100,25 @@ def success():
 
                 output = ""
 
+                pred_id = str(uuid.uuid4())[:8]              
+
                 if len(results) == 1:
-                    print("single")                    
-                    output = render_template_string(infer_baseblock+infer+infer_endblock,val1=results[0][0],val2=results[0][1])
+                    print("single")           
+                    df = df.append({'Inference Id':pred_id, 'Prediction':results[0][0], 'Confidence':results[0][1],'Timestamp':datetime.datetime.now().strftime("%c")},ignore_index=True)
+                    df1 = df.tail(5)
+                    print(df1)
+                    df1.to_csv(app.config['datalog'], mode='a',  header=False, index=False)
+                    output = render_template_string(infer_baseblock+infer+infer_endblock.replace("{{last5}}",df1.to_html(justify='center',index=False)).replace("<table","<table align=\"center\""),val1=results[0][0],val2=results[0][1],img_data=saveLocation[0])                    
                 else:                    
                     for result in results:
                         output += infer.replace("{{val1}}",result[0]).replace("{{val2}}",result[1])
-                    output = render_template_string(infer_baseblock+output+infer_endblock)
+                        df = df.append({'Inference Id':pred_id, 'Prediction':result[0], 'Confidence':result[1],'Timestamp':datetime.datetime.now().strftime("%c")},ignore_index=True)
+
+                    df1 = df.tail(5)
+                    print(df1)
+                    df1.to_csv(app.config['datalog'], mode='a', header=False, index=False)
+                    
+                    output = render_template_string(infer_baseblock+output+infer_endblock.replace("{{last5}}",df1.to_html(justify='center',index=False)).replace("<table","<table align=\"center\""))                
 
                 return output             
     except :
